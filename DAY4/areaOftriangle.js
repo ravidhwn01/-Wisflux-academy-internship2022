c202f4b1dcfd12bae183dca7a238de5895ae1f2d
ravidhwn01 <87488989+ravidhwn01@users.noreply.github.com>
2022-07-07 23:09:58 +0530

    Area Of  Triangle

---
+++ DAY4/areaOftriangle.js
@@ -0,0 +1,16 @@
+code 1:-
+
+areaOfTriangle(h,b){
+  return 1/2*(b*h);
+}
+const area = areaOfTriangle(5,6);
+console.log(area);
+
+
+code 2:-
+let x=3,y=4,z=5;
+
+let s= (x+y+z)/2;
+let c = s*((s-x)*(s-y)*(s-z));
+let area1 =  Math.sqrt(c);
+console.log(area1);
